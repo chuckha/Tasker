17331d445ed9c723004ee538c9bf93dcd61b252b
Chuck Ha <ha.chuck@gmail.com>
2013-03-17 12:00:15 -0400

    Organizes printing code better

--- dict_to_plist.py
+++ dict_to_plist.py
@@ -1,16 +1,17 @@
+from random import choice
+from string import lowercase
+
 def dict_to_plist(dictionary):
     plist = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-"""
-
+<plist version="1.0">\n"""
     plist += "  <dict>\n"
     for key, value in dictionary.iteritems():
         plist += "    <key>%s</key>\n" % key
         plist += "%s" % val_to_plist_val(value)
-    plist += "  </dict>\n</plist>"
+    plist += "  </dict>\n"
+    plist += "</plist>"
     return plist
-
 
 def val_to_plist_val(val, indent=2):
     spaces = "  " * indent
